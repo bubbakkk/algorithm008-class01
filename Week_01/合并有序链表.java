aa5051b45269d976c18c9368c7d8af3f6a4164a2
bubba <1318267157@qq.com>
2020-04-16 10:45:22 +0800

    merge two sorted LinkedList

---
+++ Week_01/合并有序链表.java
@@ -0,0 +1,20 @@
+class Solution {
+    //递归（谁先到空谁最后那个节点值小）
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
