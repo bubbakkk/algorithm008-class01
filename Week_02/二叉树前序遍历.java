44f1eb4056a8fd5280749ee6f35be4be0e2b43fb
bubba <1318267157@qq.com>
2020-04-26 18:06:44 +0800

    b_tree_preorder

---
+++ Week_02/二叉树前序遍历.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root , res);
+        return res;
+    }
+    public void dfs(TreeNode node,List<Integer> res) {
+        if (node == null) return ;
+        res.add(node.val);
+        dfs(node.left , res);
+        dfs(node.right , res);
+    }
+}
